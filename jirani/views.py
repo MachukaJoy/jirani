cb4e458a7e5f46957cc15da6796a076b0bb2e8e1
Machuka Joy <machukajoy@gmail.com>
2022-06-19 00:57:41 +0300

    ft: adds viewfunction that renders template to add a hood

--- jirani/views.py
+++ jirani/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .forms import SignupForm, UpdateProfileForm, UpdateUserForm
+from .forms import SignupForm, UpdateProfileForm, UpdateUserForm, NeighbourHoodForm
 from .models import Profile, User
 
 # Create your views here.
@@ -45,3 +45,14 @@ def edit_profile(request, username):
         prof_form = UpdateProfileForm(instance=request.user.profile)
 
     return render(request, 'editprofile.html', {'user_form': user_form, 'prof_form': prof_form})
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
