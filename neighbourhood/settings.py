20081abc031484cea501460b2f1f405e0d1c8b7a
Machuka Joy <machukajoy@gmail.com>
2022-06-18 21:35:03 +0300

    ft: add logout path

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -162,6 +162,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL="index"
 LOGOUT_REDIRECT_URL = "login"
 
 cloudinary.config(
