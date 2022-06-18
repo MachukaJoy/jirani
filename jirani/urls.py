478b4b0dbc1b7ae612c4ba89cdaef85c0b7c8558
Machuka Joy <machukajoy@gmail.com>
2022-06-18 22:09:18 +0300

    ft: adds urls to the profile templates

--- jirani/urls.py
+++ jirani/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('register/', views.register, name='register'),
   path("logout/", LogoutView.as_view(), name="logout"),
+  path('profile/<username>/', views.profile, name='profile'),
+  path('profile/<username>/edit', views.edit_profile, name='edit'),
 ]
